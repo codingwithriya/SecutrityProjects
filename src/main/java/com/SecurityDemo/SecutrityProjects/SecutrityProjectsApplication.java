4d5046b4b1c752e40b90f0eb1be5ab992b8afba9
codingwithriya <itisghoshriya@gmail.com>
2026-06-20 21:56:28 +0530

    first commit

---
+++ src/main/java/com/SecurityDemo/SecutrityProjects/SecutrityProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.SecurityDemo.SecutrityProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecutrityProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecutrityProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SecurityDemo/SecutrityProjects/SecutrityProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecurityDemo.SecutrityProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecutrityProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
