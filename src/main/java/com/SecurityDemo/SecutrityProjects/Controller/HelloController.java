ec2c218289769a2847e5410347bd8f2c100d93df
codingwithriya <itisghoshriya@gmail.com>
2026-06-21 01:36:31 +0530

    session controller and spring security architechture

--- src/main/java/com/SecurityDemo/SecutrityProjects/Controller/HelloController.java
+++ src/main/java/com/SecurityDemo/SecutrityProjects/Controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.SecurityDemo.SecutrityProjects.Controller;
 
+
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 @RestController
 public class HelloController {
@@ -9,5 +13,10 @@ public class HelloController {
     @GetMapping("/")
     public String hello(){
         return "Hello welcome to Spring boot learning session!";
+    }
+
+    @GetMapping("id")
+    public String getSessionId(HttpServletRequest hp){
+        return "Session Id:" + hp.getSession().getId();
     }
 }
